dd5ab2945a601102a83e9afb4a0662522ffdad7f
Gillys Ayres <gillys.ayres@gmail.com>
2023-11-13 17:10:55 -0300

    improved seed an uniqueness validation for cnj_number

--- app/models/legal_case.rb
+++ app/models/legal_case.rb
@@ -1,4 +1,5 @@
 class LegalCase < ApplicationRecord
   has_many :movements
   validates :cnj_number, :party_names, :origin_court, :start_date, presence: true
+  validates :cnj_number, uniqueness: true
 end
